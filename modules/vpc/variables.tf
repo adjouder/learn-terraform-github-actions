4a7321c27c772e9b0cb36ac314e4573aa19b6db7
DJOUDER <djouder@Ordinateur-portable-de-DJOUDER.local>
2024-01-21 14:48:47 +0100

    Utilisation de terraform EKS 1

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -18,3 +18,19 @@ variable "map_public_ip_on_launch" {
 variable "rt_route_cidr_block" {
 
 }
+
+
+variable "region" {
+  description = "AWS region"
+  default     = "eu-west-3"
+}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform"
+}
+
