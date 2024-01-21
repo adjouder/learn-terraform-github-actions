449dd039a5e4d4fb6d3ada7527327720d1758b71
DJOUDER <djouder@Ordinateur-portable-de-DJOUDER.local>
2024-01-21 14:54:06 +0100

    Utilisation de terraform EKS 3

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -22,18 +22,3 @@ variable "scaling_min_size" {}
 variable "instance_types" {}
 
 variable "key_pair" {}
-
-variable "region" {
-  description = "AWS region"
-  default     = "eu-west-3"
-}
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
-}
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
-}
-
